d94d6596abb8a0a612af2bb1d6c22ac8e090c48c
Kevin <kevinyehdev@gmail.com>
2024-10-04 12:04:31 -0700

    Improved lkas_status for Toyota

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -233,9 +233,10 @@ class CarController(CarControllerBase):
         send_ui = True
 
       if self.frame % 20 == 0 or send_ui:
+        lat_available = frogpilot_toggles.always_on_lateral and CS.out.cruiseState.available # TODO: and not "always_on_lateral_disabled" by LTA button
         can_sends.append(toyotacan.create_ui_command(self.packer, steer_alert, pcm_cancel_cmd, hud_control.leftLaneVisible,
                                                      hud_control.rightLaneVisible, hud_control.leftLaneDepart,
-                                                     hud_control.rightLaneDepart, CC.enabled, CS.lkas_hud, lat_active))
+                                                     hud_control.rightLaneDepart, lat_available, CS.lkas_hud, lat_active, CS.out.steeringPressed))
 
       if (self.frame % 100 == 0 or send_ui) and (self.CP.enableDsu or self.CP.flags & ToyotaFlags.DISABLE_RADAR.value):
         can_sends.append(toyotacan.create_fcw_command(self.packer, fcw_alert))
